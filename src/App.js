969e2d7b44c406e11ad524e27585c15d1594d5d3
No Class Post <noclasspost@Carloss-iMac.local>
2020-12-29 10:28:18 -0800

    transferred user state to context

--- src/App.js
+++ src/App.js
@@ -13,23 +13,10 @@ import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
   const [repos, setRepos] = useState([])
   const [loading, setLoading] = useState(false)
-  const [alert, setAlert] = useState(null)
-
-
-  // Get single Github user
-  const getUser = async (username) => {
-    setLoading(true)
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUser(res.data)
-    setLoading(false)
-  }
-
+  const [alert, setAlert] = useState(null) 
+  
   // Get users repos
   const getUserRepos = async (username) => {
     setLoading(true)
@@ -39,13 +26,7 @@ const App = () => {
     setRepos(res.data)
     setLoading(false)
   }
-
-  // Clear users from state
-  const clearUsers = () => {
-    setUsers([])
-    setLoading(false)
-  }
-
+  
   // Set Alert
   const showAlert = (msg, type) => {
     setAlert({ msg, type })
@@ -64,11 +45,9 @@ const App = () => {
             <Route exact path='/' render= {props => (
               <Fragment>
                 <Search  
-                clearUsers={clearUsers}
-                showClear={users.length > 0 ? true : false}
                 setAlert={showAlert}
                 />
-                <Users loading={loading} users={users} /> 
+                <Users /> 
               </Fragment>
             )}
             />
@@ -76,13 +55,12 @@ const App = () => {
             <Route exact path='/user/:login' render={props => (
               <User 
               {...props } 
-              getUser={getUser} 
               getUserRepos={getUserRepos} 
-              user={user} 
               repos={repos}
-              loading={loading} />
-            )} />
-          </Switch>
+               />
+            )}
+         />
+        </Switch>
         </div>
       </div>
       </Router>
